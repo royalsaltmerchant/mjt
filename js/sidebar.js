1afc87cf2f7257bf25657ac3261aa90e23f58bd1
Julian Ranieri <68655515+royalsaltmerchant@users.noreply.github.com>
2024-02-27 15:32:11 -0800

    about, albums, navigation

---
+++ js/sidebar.js
@@ -0,0 +1,54 @@
+// slide out sidebar
+function SideBar(props) {
+  this.props = props
+
+  this.render = function() {
+    this.elem = document.getElementById(this.props.id)
+
+    var html =  /* html */`
+      <div class="sidebar" id="sidebar">
+        <div class="sidebar-items" id="sidebar-items">
+          <a href="/about"><div class="sidebar-items-item">About</div></a>
+          <hr>
+          <a href="/albums"><div class="sidebar-items-item">Albums</div></a>    
+        </div>
+      </div>
+    `
+    this.elem.innerHTML = html
+
+    // get self
+    var self = this
+    // get items
+    this.items = document.getElementById('sidebar-items')
+    // get hamburger button and listen to mouse down
+    var hamburger = document.getElementById('hamburger')
+    hamburger.addEventListener('mousedown', function() {
+      event.stopPropagation()
+      if(self.isVisible) {
+        self.hide()
+      } else {
+        self.show()
+      }
+    })
+    // hide if click anywhere else
+    document.addEventListener('mousedown', function() {
+      self.hide()
+    })
+  }
+
+  // hide sidebar
+  this.hide = function() {
+    this.isVisible = false
+    this.items.style.transform = 'translate(200px, 0px)'
+  }
+  // show sidebar
+  this.show = function() {
+    this.isVisible = true
+    this.items.style.transform = 'translate(0px, 0px)'
+  }
+
+  this.render()
+  return this
+}
+
+var sidebar = new SideBar({id: 'mobile-menu'})
